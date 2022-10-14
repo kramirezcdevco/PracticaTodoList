0ae922f9073dffe0cc10b832dd8269e8b0703ba4
Karen Ramirez <kramirezc@devco.com.co>
2022-10-14 11:48:14 -0500

    Proyecto base

---
+++ src/main/java/org/karenramirez/todolist/TodoListApp.java
@@ -0,0 +1,10 @@
+package org.karenramirez.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class TodoListApp {
+    public static void main(String[] args){
+        SpringApplication.run(TodoListApp.class, args);
+    }
+}
