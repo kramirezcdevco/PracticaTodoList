c570cfd5cc754449b841dd4fbff7bf936e380a7d
Karen Ramirez <kramirezc@devco.com.co>
2022-10-20 17:54:50 -0500

    Ajustes en la base del proyecto

---
+++ src/test/java/org/karenramirez/todolist/TodoListAppTest.java
@@ -0,0 +1,12 @@
+package org.karenramirez.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TodoListAppTest {
+    @Test
+    void contextLoads() {
+
+    }
+}
